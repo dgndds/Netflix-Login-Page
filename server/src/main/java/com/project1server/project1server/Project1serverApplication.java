7f7de8f46a073d6c723458e4c7037536209d968f
eaktas2 <eaktas2@gmail.com>
2022-02-11 23:28:40 +0300

    Added LDAP login authentication with Spring Boot

---
+++ server/src/main/java/com/project1server/project1server/Project1serverApplication.java
@@ -0,0 +1,13 @@
+package com.project1server.project1server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1serverApplication.class, args);
+	}
+
+}
